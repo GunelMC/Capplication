c3d70ed4086c8dc3caf4e4f4255241a9e7107d0e
Gunel Mehdiyeva-Chau <gunel.mehdiyev@gmail.com>
2021-08-19 18:10:13 +0100

    User can favourite cat images

--- app.rb
+++ app.rb
@@ -1,6 +1,7 @@
 require 'sinatra/base'
 require 'sinatra/reloader'
 require 'http'
+require 'dotenv/load'
 
 class CatManager < Sinatra::Base
   configure :development do
@@ -9,10 +10,20 @@ class CatManager < Sinatra::Base
 
   get '/' do
     json_response = HTTP.headers(:accept => 'application/json').get("https://api.thecatapi.com/v1/images/search?limit=3").to_s
-    parsed_json = JSON.parse(json_response)
-    @image_urls = parsed_json.map { |image| image['url'] }
+    @images = JSON.parse(json_response)
     erb :index
   end
+
+  post '/favourites' do
+    HTTP.headers(:"content-type"=> 'application/json', :"x-api-key"=> 'e6d7027d-a3a6-48a7-b14a-84b646116158').post("https://api.thecatapi.com/v1/favourites", :body => { :image_id => params["image_id"] }.to_json ).to_s
+    redirect '/'
+  end
+
+  get '/favourites' do
+    json_response = HTTP.headers(:accept => 'application/json', :"x-api-key"=> ENV["API_KEY"]).get("https://api.thecatapi.com/v1/favourites").to_s
+    @favourites = JSON.parse(json_response)
+    erb :favourites
+  end 
 
   run! if app_file == $0
 end
---
+++ spec/features/favourite_cats_spec.rb
@@ -0,0 +1,47 @@
+feature 'Favouriting cat images' do
+  scenario 'A user can favourite a cat image' do
+
+    # mock the API call 
+    stubbed_response_body = [ 
+  {
+    breeds: [],id: "343", url: "https://cdn2.thecatapi.com/images/343.gif",width: 100,height: 100
+  },
+  {
+    breeds: [], 
+    categories: [
+      {
+        id: 14,
+        name: "sinks"
+      }
+    ],
+    id: "MTc5NjcyNQ",
+    url: "https://cdn2.thecatapi.com/images/MTc5NjcyNQ.jpg",
+    width: 640,
+    height: 640
+  },
+  {
+    breeds: [
+      
+    ],
+    id: "uSdQBzCfg",
+    url: "https://cdn2.thecatapi.com/images/uSdQBzCfg.jpg",
+    width: 509,
+    height: 340
+  }
+]
+    
+    stubbed_response_body_json = JSON.generate(stubbed_response_body)
+
+    stub_request(:get, "https://api.thecatapi.com/v1/images/search?limit=3").
+      with( headers: { 'Accept'=>'application/json', 'Connection'=>'close', 'Host'=>'api.thecatapi.com', 'User-Agent'=>'http.rb/5.0.1' }).
+      to_return(status: 200, body: stubbed_response_body_json, headers: {})
+  
+    visit('/')
+    find(".my_button", match: :first).click
+    visit('/favourites')
+
+    expect(page).to have_content("img[src*='https://cdn2.thecatapi.com/images/343.gif']")
+    expect(page).not_to have_css("img[src*='https://cdn2.thecatapi.com/images/MTc5NjcyNQ.jpg']")
+    expect(page).not_to have_css("img[src*='https://cdn2.thecatapi.com/images/uSdQBzCfg.jpg']")
+  end
+end
